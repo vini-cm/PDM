abbd322b69743bb8fb1502f276b194228c3a11ed
unknown <vinicius.nb07@aluno.ifsc.edu.br>
2025-11-17 09:14:50 -0300

    SQL

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -1,12 +1,18 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -14,19 +20,67 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.ArrayList;
 
 public class MainActivity2 extends Activity {
+    SQLiteDatabase db;
+    Button button;
+    EditText editText;
     ListView listView;
-    ArrayList<String> nomes;
-    PlanetaController planetaController;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editTextText);
         listView = findViewById(R.id.listView);
-        PlanetaController planetaController = new PlanetaController();
-        PlanetaAdapter adapter = new PlanetaAdapter(this, R.layout.item_lista, planetaController.getPlaneta());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
-        listView.setAdapter(adapter);
+        db = openOrCreateDatabase("meu_banco.db", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS notas" +
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT, titulo VARCHAR, txt TEXT);");
+
+        carregarListagem();
+
+        button.setOnClickListener(v -> {
+            String titulo = editText.getText().toString();
+            ContentValues cv = new ContentValues();
+            cv.put("titulo", titulo);
+            db.insert("notas",null, cv);
+            carregarListagem();
+        });
+
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            String titulo = (String) parent.getItemAtPosition(position);
+            Intent intent = new Intent(MainActivity2.this, ExibeItem.class);
+            intent.putExtra("titulo",titulo);
+            startActivity(intent);
+
+        });
+
+
     }
+
+    public void carregarListagem() {
+        ArrayList<String> titulos = new ArrayList<String>();
+        Cursor cursor = db.rawQuery("SELECT * FROM notas", null);
+        cursor.moveToFirst();
+
+        while(!cursor.isAfterLast()) {
+            String titulo = cursor.getString(cursor.getColumnIndex("titulo"));
+            titulos.add(titulo);
+            cursor.moveToNext();
+        }
+
+        ArrayAdapter<String> titulosAdapter = new ArrayAdapter<>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                titulos
+        );
+
+        listView.setAdapter(titulosAdapter);
+    }
+
 }
