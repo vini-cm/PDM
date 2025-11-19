f16a33d9e625b33a57e627235afe55d473eec6b6
Vinicius.nb07 <vinicius@gmail.com>
2025-11-19 11:46:13 -0300

    TentandoRefazer

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        EditText edt = findViewById(R.id.editTextText);
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(v -> {
+            String texto = edt.getText().toString();
+
+            if (texto.isEmpty()) {
+                edt.setError("Digite um número.");
+                return;
+            }
+
+            double valor = Double.parseDouble(texto);
+
+            Intent intent = new Intent(MainActivity.this, resultado.class);
+            intent.putExtra("VALOR", valor);
+            startActivity(intent);
+        });
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/resultado.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class resultado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_resultado);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent i = getIntent();
+        Bundle b = i.getExtras();
+        double base = b.getDouble("VALOR");
+
+        TextView resultados = findViewById(R.id.resultados);
+
+        String resultado = "";
+        for (int j = 1; j <= 10; j++) {
+            resultado += base + " * " + j + " = " + (base * j) + "\n";
+        }
+
+        resultados.setText(resultado);
+    }
+}
