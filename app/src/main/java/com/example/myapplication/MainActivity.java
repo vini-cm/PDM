8078020e1fa15fed6a1144a4b910250a0e95f3a4
vinicius.nb07 <vinicius.nb07@aluno.ifsc.edu.br>
2025-11-28 16:53:04 -0300

    Cicl

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,35 +1,66 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ListView;
+import android.util.Log;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView lv;
-    Button btnAtualizar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        lv = findViewById(R.id.lv);
-        btnAtualizar = findViewById(R.id.btnAtualizar);
-
-        // Aqui você coloca a lógica do botão e do ListView
     }
+
+    @Override
+    protected void onStart() {
+        Log.d("lifecycle","Start");
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("lifecycle","Pause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("lifecycle","Restart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("lifecycle","Stop");
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("lifecycle","Destroy");
+
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("lifecycle","Resume");
+
+        super.onResume();
+    }
+
+
 }
