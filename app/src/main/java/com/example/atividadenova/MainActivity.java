6220c39aa7c15bac0ce73d51b91da481828c6ca2
vinicius.nb07 <vinicius.nb07@aluno.ifsc.edu.br>
2025-11-28 16:11:06 -0300

    IMC

--- app/src/main/java/com/example/atividadenova/MainActivity.java
+++ app/src/main/java/com/example/atividadenova/MainActivity.java
@@ -3,29 +3,44 @@ package com.example.atividadenova;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.atividadenova.R;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        button.setOnClickListener( v ->{
-            Intent intent= new Intent( this, Imc_resultado.class);
+
+        Button button = findViewById(R.id.btnCalcular);
+        EditText campoPeso = findViewById(R.id.editPes);
+        EditText campoAlt = findViewById(R.id.editAlt);
+
+        button.setOnClickListener(v -> {
+
+            String txtPeso = campoPeso.getText().toString();
+            String txtAlt = campoAlt.getText().toString();
+
+            if (txtPeso.isEmpty() || txtAlt.isEmpty()) {
+                campoPeso.setError("Digite o peso");
+                campoAlt.setError("Digite a altura");
+                return;
+            }
+
+            Intent intent = new Intent(this, Resultado_imc.class);
+            Bundle bundle = new Bundle();
+
+            bundle.putDouble("peso", Double.parseDouble(txtPeso));
+            bundle.putDouble("altura", Double.parseDouble(txtAlt));
+
+            intent.putExtras(bundle);
             startActivity(intent);
-        });
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
         });
     }
 }
---
+++ app/src/main/java/com/example/atividadenova/Resultado_imc.java
@@ -0,0 +1,48 @@
+package com.example.atividadenova;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.atividadenova.R;
+
+public class Resultado_imc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imc_resultado); // <- precisa bater com o XML
+
+        Intent i = getIntent();
+        Bundle b = i.getExtras();
+
+        double peso = b.getDouble("peso");
+        double altura = b.getDouble("altura");
+
+        TextView tvResultado = findViewById(R.id.textResultado);
+        ImageView imageView = findViewById(R.id.viewPerfil);
+
+        double imc = peso / (altura * altura);
+        tvResultado.setText(String.format("Seu IMC: %.2f", imc));
+
+        if (imc < 18.5) {
+            imageView.setImageResource(R.drawable.abaixopeso);
+        } else if (imc < 24.9) {
+            imageView.setImageResource(R.drawable.normal);
+        } else if (imc < 29.9) {
+            imageView.setImageResource(R.drawable.sobrepeso);
+        } else if (imc < 34.9) {
+            imageView.setImageResource(R.drawable.obesidade1);
+        } else if (imc < 39.9) {
+            imageView.setImageResource(R.drawable.obesidade2);
+        } else {
+            imageView.setImageResource(R.drawable.obesidade3);
+        }
+
+        // Botão Voltar
+        findViewById(R.id.btnReturn).setOnClickListener(v -> finish());
+    }
+}
