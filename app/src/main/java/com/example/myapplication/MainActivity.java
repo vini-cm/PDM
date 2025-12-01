3b1ba37e3f4ee3d86d3d5d388bad682dd09f49ef
Vinicius.nb07 <vinicius.nb07@aluno.ifsc.edu.br>
2025-12-01 08:50:47 -0300

    Ola_bilingue

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,37 +1,13 @@
 package com.example.myapplication;
 
-import android.os.Bundle;
-import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView tv;
-    int x = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-        tv = findViewById(R.id.tv);
-        tv.setText("Clique me");
-
-        tv.setOnClickListener(v -> {
-            tv.setText(String.valueOf(x));
-            x++;
-        });
     }
 }
